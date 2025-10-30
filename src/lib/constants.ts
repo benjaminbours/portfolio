0c1ee569c420a6b98355a8befde04eb0ab72707a
Benjamin Bours <boursbenjamin@gmail.com>
2025-10-30 17:35:18 +0000

    update base url

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,6 +1,6 @@
 export const SITE_NAME = "Benjamin Bours";
 export const SITE_URL = "https://bours.dev";
-export const BASE_PATH = "/bbblog";
+export const BASE_PATH = "/portfolio";
 export const HOME_OG_IMAGE_URL = `${SITE_URL}${BASE_PATH}/assets/or_image.svg`;
 export const LOCALES = ["en", "fr"];
 export const DEFAULT_LOCALE = "en";
